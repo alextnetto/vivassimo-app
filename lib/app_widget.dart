4d9952f7d19ae5fe7c82ac9b3e6c449249d67227
Alextnetto <alex.t.netto@gmail.com>
2021-10-18 14:42:38 -0300

    feature(register): birthday page and validation

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/app_controller.dart';
 import 'package:my_app/home_page.dart';
+import 'package:my_app/pages/register/birthday.dart';
 import 'package:my_app/pages/register/cpf_page.dart';
 import 'package:my_app/pages/register/accept_terms.dart';
 import 'package:my_app/pages/register/email.dart';
@@ -35,6 +36,7 @@ class AppWidget extends StatelessWidget {
               '/register/passwordConfirmation': (context) =>
                   PasswordConfirmPage(),
               '/register/cpf': (context) => CpfPage(),
+              '/register/birthday': (context) => BirthdayPage(),
             },
           ),
         );
