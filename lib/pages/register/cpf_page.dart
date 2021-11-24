861b089f558dfe40ab31049002b39cfb24145cd4
Alextnetto <alex.t.netto@gmail.com>
2021-11-24 18:53:18 -0300

    refactor(register): CPF page

--- lib/pages/register/cpf_page.dart
+++ lib/pages/register/cpf_page.dart
@@ -3,86 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:my_app/components/button_back.dart';
-import 'package:my_app/components/button_1.dart';
+import 'package:my_app/components/button_confirm.dart';
 import 'package:my_app/components/input_decoration.dart';
 import 'package:my_app/components/text_style.dart';
 import 'package:my_app/config/style.dart';
 import 'package:my_app/models/register/user.dart';
 
-class CpfPage extends StatelessWidget {
+class CpfPage extends StatefulWidget {
   const CpfPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: VivassimoTheme.white,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: [
-            Hero(
-              tag: 'registerAppBar',
-              child: Container(
-                height: 130,
-                color: VivassimoTheme.blue,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ButtonBack(),
-                        // texto
-                        Padding(
-                          padding: const EdgeInsets.only(right: 30),
-                          child: Text(
-                            'Criar uma conta',
-                            style: customTextStyle(
-                              FontWeight.w700,
-                              18,
-                              VivassimoTheme.purpleActive,
-                            ),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              width: 300,
-              child: Text(
-                'Digite o número do seu CPF',
-                style: customTextStyle(
-                  FontWeight.w700,
-                  23,
-                  VivassimoTheme.purpleActive,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            CpfForm(),
-          ],
-        ),
-      ),
-    );
+  CpfPageState createState() {
+    return CpfPageState();
   }
 }
 
-class CpfForm extends StatefulWidget {
-  const CpfForm({Key? key}) : super(key: key);
-
-  @override
-  CpfFormState createState() {
-    return CpfFormState();
-  }
-}
-
-class CpfFormState extends State<CpfForm> {
+class CpfPageState extends State<CpfPage> {
   final _formKey = GlobalKey<FormState>();
 
   var maskFormatter = MaskTextInputFormatter(
@@ -94,54 +30,114 @@ class CpfFormState extends State<CpfForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          SizedBox(
-            width: 324,
-            height: 90,
-            child: TextFormField(
-              keyboardType: TextInputType.number,
-              inputFormatters: [maskFormatter],
-              onSaved: (value) {
-                RegisterUser.instance.cpf = maskFormatter.getUnmaskedText();
-              },
-              validator: (value) {
-                if (!CPFValidator.isValid(value)) {
-                  return 'CPF inválido';
-                }
-                return null;
-              },
-              decoration: customInputDecoration1('Digite aqui o CPF'),
-              textAlign: TextAlign.center,
-              style: customTextStyle(
-                FontWeight.w700,
-                18,
-                VivassimoTheme.purpleActive,
+    return Scaffold(
+      body: Container(
+        color: VivassimoTheme.white,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Hero(
+                  tag: 'registerAppBar',
+                  child: Container(
+                    height: 130,
+                    color: VivassimoTheme.blue,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ButtonBack(),
+                            // texto
+                            Padding(
+                              padding: const EdgeInsets.only(right: 30),
+                              child: Text(
+                                'Criar uma conta',
+                                style: customTextStyle(
+                                  FontWeight.w700,
+                                  18,
+                                  VivassimoTheme.purpleActive,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  width: 300,
+                  child: Text(
+                    'Digite o número do seu CPF',
+                    style: customTextStyle(
+                      FontWeight.w700,
+                      23,
+                      VivassimoTheme.purpleActive,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: 324,
+                        height: 90,
+                        child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [maskFormatter],
+                          onSaved: (value) {
+                            RegisterUser.instance.cpf =
+                                maskFormatter.getUnmaskedText();
+                          },
+                          validator: (value) {
+                            if (!CPFValidator.isValid(value)) {
+                              return 'CPF inválido';
+                            }
+                            return null;
+                          },
+                          decoration:
+                              customInputDecoration1('Digite aqui o CPF'),
+                          textAlign: TextAlign.center,
+                          style: customTextStyle(
+                            FontWeight.w700,
+                            18,
+                            VivassimoTheme.purpleActive,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Hero(
+              tag: 'registerButtonConfirm',
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ButtonConfirm(
+                  label: 'Continuar',
+                  primary: VivassimoTheme.green,
+                  onPrimary: VivassimoTheme.white,
+                  borderColor: VivassimoTheme.white,
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      Navigator.of(context).pushNamed('/register/birthday');
+                    }
+                  },
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: SizedBox(
-              width: 324,
-              height: 60,
-              child: CustomButton1(
-                label: 'Continuar',
-                primary: VivassimoTheme.green,
-                onPrimary: VivassimoTheme.white,
-                borderColor: VivassimoTheme.white,
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    Navigator.of(context).pushNamed('/register/birthday');
-                  }
-                },
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
