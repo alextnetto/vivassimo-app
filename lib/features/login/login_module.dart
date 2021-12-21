d828db0e5a05ee4fc86b7744caa848a168f6920c
RodriguesYan <rds_06@hotmail.com>
2021-12-21 12:48:14 -0300

    login mocked

--- lib/features/login/login_module.dart
+++ lib/features/login/login_module.dart
@@ -5,13 +5,15 @@ import 'package:my_app/features/login/external/datasources/login_datasource.dart
 import 'package:my_app/features/login/infra/repositories/login_repository.dart';
 import 'package:my_app/features/login/presentation/stores/login_store.dart';
 
+import 'external/datasources/login_datasource_mocked.dart';
+
 class LoginModule extends Module {
   @override
   List<Bind> get binds {
     // var http = HttpService();
     return [
       Bind.factory((i) => HttpService()),
-      Bind.factory((i) => LoginDatasource(i())),
+      Bind.factory((i) => LoginDatasourceMocked(i())),
       Bind.factory((i) => LoginRepository(i())),
       Bind.factory((i) => LoginUsecase(i(), i())),
       Bind.factory((i) => LoginStore(i())),
