a2852faf812b7ecb2d4f683bac9e01438d734a0d
RodriguesYan <rds_06@hotmail.com>
2021-12-29 15:56:45 -0300

    Register - unit tests in repository layer

--- test/features/register/infra/repositories/register_repository_test.dart
+++ test/features/register/infra/repositories/register_repository_test.dart
@@ -4,9 +4,13 @@ import 'package:mocktail/mocktail.dart';
 import 'package:my_app/features/register/domain/errors/register_errors.dart';
 import 'package:my_app/features/register/infra/datasources/i_register_datasource.dart';
 import 'package:my_app/features/register/infra/models/request/check_existing_user_request_model.dart';
+import 'package:my_app/features/register/infra/models/request/register_user_request_model.dart';
 import 'package:my_app/features/register/infra/models/request/send_otp_request_model.dart';
+import 'package:my_app/features/register/infra/models/request/verify_otp_request_model.dart';
 import 'package:my_app/features/register/infra/models/response/check_existing_user_response_model.dart';
+import 'package:my_app/features/register/infra/models/response/register_user_response_model.dart';
 import 'package:my_app/features/register/infra/models/response/send_otp_response_model.dart';
+import 'package:my_app/features/register/infra/models/response/verify_otp_response_model.dart';
 import 'package:my_app/features/register/infra/repositories/register_repository.dart';
 
 class RegisterDatasourceMock extends Mock implements IRegisterDatasource {}
@@ -73,21 +77,43 @@ void main() {
 
   group('Validate OTP - ', () {
     test('Should return success if otp code is validated', () async {
-      SendOtpRequestModel requestModel = SendOtpRequestModel(phoneNumber: '11988888888');
+      VerifyOtpRequestModel requestModel = VerifyOtpRequestModel(phoneNumber: '11988888888', token: '777777');
 
-      when(() => datasource.sendOtp(requestModel)).thenAnswer((_) async => SendOtpResponseModel(success: true));
+      when(() => datasource.verifyOtp(requestModel)).thenAnswer((_) async => VerifyOtpResponseModel(success: true));
 
-      var result = await repository.sendOtp(requestModel);
+      var result = await repository.verifyOtp(requestModel);
 
-      expect(result.fold(id, id), SendOtpResponseModel(success: true));
+      expect(result.fold(id, id), VerifyOtpResponseModel(success: true));
     });
 
-    test('Should return success if user doesn\'t receive the otp code', () async {
-      SendOtpRequestModel requestModel = SendOtpRequestModel(phoneNumber: '11988888888');
+    test('Should return success otp code is wrong', () async {
+      VerifyOtpRequestModel requestModel = VerifyOtpRequestModel(phoneNumber: '11988888888', token: '666666');
 
-      when(() => datasource.sendOtp(requestModel)).thenThrow(RegisterDatasourceError(message: 'fff'));
+      when(() => datasource.verifyOtp(requestModel)).thenThrow(RegisterDatasourceError(message: 'fff'));
 
-      var result = await repository.sendOtp(requestModel);
+      var result = await repository.verifyOtp(requestModel);
+
+      expect(result.fold(id, id), isA<RegisterDatasourceError>());
+    });
+  });
+
+  group('Register User - ', () {
+    test('Should return success if user is registered successfully', () async {
+      RegisterUserRequestModel requestModel = RegisterUserRequestModel();
+
+      when(() => datasource.register(requestModel)).thenAnswer((_) async => RegisterUserResponseModel(success: true));
+
+      var result = await repository.register(requestModel);
+
+      expect(result.fold(id, id), RegisterUserResponseModel(success: true));
+    });
+
+    test('Should return an error is user is not registered', () async {
+      RegisterUserRequestModel requestModel = RegisterUserRequestModel();
+
+      when(() => datasource.register(requestModel)).thenThrow(RegisterDatasourceError(message: 'fff'));
+
+      var result = await repository.register(requestModel);
 
       expect(result.fold(id, id), isA<RegisterDatasourceError>());
     });
